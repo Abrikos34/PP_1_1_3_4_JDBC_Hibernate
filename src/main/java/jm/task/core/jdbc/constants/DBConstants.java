5c597f2a8ae3915fadc9079de3ecd9055cdcb674
Aleksey <bibuzi32@gmail.com>
2024-12-19 22:47:43 +0300

    Добавлен .gitignore для исключения ненужных файлов

--- src/main/java/jm/task/core/jdbc/constants/DBConstants.java
+++
@@ -1,9 +0,0 @@
-package jm.task.core.jdbc.constants;
-
-public interface DBConstants {
-    String DB_HOST = "localhost";
-    String DB = "user_database";
-    String USER = "root";
-    String PASSWORD = "bibuzi34";
-    String PORT = "3306";
-}
