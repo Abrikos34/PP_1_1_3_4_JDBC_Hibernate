3da72f4d9c2323a909a613af8aa172826fa8b6bf
Aleksey <bibuzi32@gmail.com>
2024-12-24 18:43:55 +0300

    Refactor Util.java: replaced static initialization with Singleton pattern

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -13,42 +13,49 @@ public class Util {
     private static final String USER = "root";
     private static final String PASSWORD = "bibuzi34";
 
-    private static final SessionFactory sessionFactory;
+    private static SessionFactory sessionFactory;
     private static Connection connection;
-
-    static {
-        try {
-            sessionFactory = new Configuration()
-                    .setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver")
-                    .setProperty("hibernate.connection.url", DB_URL)
-                    .setProperty("hibernate.connection.username", USER)
-                    .setProperty("hibernate.connection.password", PASSWORD)
-                    .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect")
-                    .setProperty("hibernate.show_sql", "true")
-                    .setProperty("hibernate.hbm2ddl.auto", "update")
-                    .addAnnotatedClass(User.class)
-                    .buildSessionFactory();
-            System.out.println("Hibernate SessionFactory initialized in Util!");
-        } catch (Throwable ex) {
-            System.err.println("Failed to initialize Hibernate SessionFactory in Util: " + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
 
     public static Connection getConnection() {
         if (connection == null) {
-            try {
-                connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
-                System.out.println("JDBC Connection established!");
-            } catch (SQLException e) {
-                System.out.println("Failed to establish JDBC Connection...");
-                throw new RuntimeException(e);
+            synchronized (Util.class) {
+                if (connection == null) {
+                    try {
+                        connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+                        System.out.println("JDBC Connection established!");
+                    } catch (SQLException e) {
+                        System.out.println("Failed to establish JDBC Connection...");
+                        throw new RuntimeException(e);
+                    }
+                }
             }
         }
         return connection;
     }
 
     public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            synchronized (Util.class) {
+                if (sessionFactory == null) {
+                    try {
+                        sessionFactory = new Configuration()
+                                .setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver")
+                                .setProperty("hibernate.connection.url", DB_URL)
+                                .setProperty("hibernate.connection.username", USER)
+                                .setProperty("hibernate.connection.password", PASSWORD)
+                                .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect")
+                                .setProperty("hibernate.show_sql", "true")
+                                .setProperty("hibernate.hbm2ddl.auto", "update")
+                                .addAnnotatedClass(User.class)
+                                .buildSessionFactory();
+                        System.out.println("Hibernate SessionFactory initialized!");
+                    } catch (Throwable ex) {
+                        System.err.println("Failed to initialize Hibernate SessionFactory: " + ex);
+                        throw new ExceptionInInitializerError(ex);
+                    }
+                }
+            }
+        }
         return sessionFactory;
     }
 
@@ -56,6 +63,7 @@ public class Util {
         if (connection != null) {
             try {
                 connection.close();
+                connection = null;
                 System.out.println("JDBC Connection closed!");
             } catch (SQLException e) {
                 throw new RuntimeException(e);
@@ -66,9 +74,11 @@ public class Util {
     public static void closeSessionFactory() {
         if (sessionFactory != null) {
             sessionFactory.close();
-            System.out.println("Hibernate SessionFactory closed in Util!");
+            sessionFactory = null;
+            System.out.println("Hibernate SessionFactory closed!");
         }
     }
 }
+
 
 
